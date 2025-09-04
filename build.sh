a3813afe347dcf545eac4b765158f936722ade5d
RohitShah1706 <rlshah03@gmail.com>
2025-09-04 09:17:48 +0530

    chore: update docker-compose to run rasp-rbac

--- build.sh
+++ build.sh
@@ -3,5 +3,8 @@
 # cd "/home/ctri/Desktop/RASPutin/rasp-fe/Drag-Drop(RFE)"
 # docker build -t 172.16.202.56:5000/rasp-designer-fe:latest .
 
-cd "/home/ctri/Desktop/RASPutin/rasp-fe/Drag-Drop(RBE)"
-docker build -t 172.16.202.56:5000/rasp-designer-be:latest .
+# cd "/home/ctri/Desktop/RASPutin/rasp-fe/Drag-Drop(RBE)"
+# docker build -t 172.16.202.56:5000/rasp-designer-be:latest .
+
+cd "/home/ctri/Desktop/RASPutin/RASP-RBAC"
+docker build -t 172.16.202.56:5000/rasp-rbac:latest .
